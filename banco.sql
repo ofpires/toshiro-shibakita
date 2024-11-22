8fd0f9c9869ff16c6d224898c6cd79f23b5f8e40
oscarfpires <oscarfpires@gmail.com>
2024-11-22 11:12:10 -0300

    Melhora da estrutura da tabela e alteração no script php

--- banco.sql
+++ banco.sql
@@ -1,8 +1,8 @@
 CREATE TABLE dados (
-    AlunoID int,
-    Nome varchar(50),
-    Sobrenome varchar(50),
+    AlunoID int not null auto_increment,
+    Nome varchar(50) not null,
+    Sobrenome varchar(50) not null,
     Endereco varchar(150),
     Cidade varchar(50),
-    Host varchar(50)
+    Host varchar(50) not null
 );
